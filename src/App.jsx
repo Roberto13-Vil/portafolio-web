b0f0ce0d457a560989af0dad7cef0b320c7e2142
Roberto13-Vil <robertvilchis787@gmail.com>
2025-06-26 15:22:43 -0600

    project_1

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route index element={<Home />} />
-          <Route path="/project_1" element={<Project_1 />} />
+          <Route path="project_1" element={<Project_1 />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
