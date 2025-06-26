ed808f49920c10809d40846f73d4abbfc22ee6bc
Roberto13-Vil <robertvilchis787@gmail.com>
2025-06-26 15:06:54 -0600

    project_1

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { NotFound } from "./pages/NotFound";
 import { Toaster } from "@/components/ui/toaster";
+import Project_1 from "./pages/Porject1";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route index element={<Home />} />
           <Route path="*" element={<NotFound />} />
+          <Route path="/project_1" element={<Project_1 />} />
         </Routes>
       </BrowserRouter>
     </>
